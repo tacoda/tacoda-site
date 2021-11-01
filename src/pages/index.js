2c950a01a96b3a90269d2b1a2a77c65c4a2643e8
tacoda <tacoda@hey.com>
2021-10-31 20:20:19 -0400

    Update link descriptions and add email

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,36 +100,43 @@ const links = [
     text: "GitHub",
     url: "https://github.com/tacoda",
     description:
-      "My GitHub profile.",
+      "My code repositories",
     color: "#E95800",
   },
   {
     text: "Exercism",
     url: "https://exercism.org/profiles/tacoda",
     description:
-      "My Exercism profile.",
+      "Learning and mentorship",
     color: "#1099A8",
   },
   {
     text: "Dev.to",
     url: "https://dev.to/tacoda",
     description:
-      "My Dev.to profile.",
+      "My writings",
     color: "#BC027F",
   },
   {
     text: "Stack Overflow",
     url: "https://stackoverflow.com/story/tacoda",
     description:
-      "My Developer Story",
+      "My developer story",
     color: "#0D96F2",
   },
   {
     text: "Twitter",
     url: "https://twitter.com/tacoda_dev",
     description:
-      "My Twitter profile.",
+      "My twitter profile",
     color: "#8EB814",
+  },
+  {
+    text: "Email",
+    url: "mailto:tacoda@hey.com",
+    description:
+      "Email me at: tacoda@hey.com",
+    color: "#34B8DE",
   },
 ]
 
@@ -170,6 +177,7 @@ const IndexPage = () => {
           ))}
         </ul>
       </div>
+      <hr />
       <img
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
